e7d0035c471c3b07901ce20ba5d714b85a67fdb5
Hsien-Hsiu Liao <hsien-hsiu.liao@yandex.com>
2022-03-10 12:43:55 -0800

    add msg

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,37 @@
 fn main() {
-    println!("Hello, world!");
+    /*
+    function rot13(str) { // LBH QVQ VG!
+    var num;
+    var newstr="";
+    var decoded="";
+    for (let i=0;i<str.length ;i++){
+      num= str.charCodeAt(i);
+   // num = num-13; a=65 Z=90
+      if (num>77 && num <91){
+    decoded += String.fromCharCode(num-13);
+      //decoded += newstr;
+      }
+      else if (num>64 && num <78){
+    decoded += String.fromCharCode(num+13);
+      //decoded += newstr;
+      }
+      else {
+          decoded += String.fromCharCode(num);
+  
+      }  
+    }
+    return decoded;
+  }
+
+  rot13("SERR PBQR PNZC") should decode to the string FREE CODE CAMP
+
+rot13("SERR CVMMN!") should decode to the string FREE PIZZA!
+
+rot13("SERR YBIR?") should decode to the string FREE LOVE?
+
+rot13("GUR DHVPX OEBJA SBK WHZCF BIRE GUR YNML QBT.") should decode to the string THE QUICK BROWN FOX JUMPS OVER THE LAZY DOG.
+    */
+    let decoded = String::from("decoded");
+    println!("Enter a ROT13 encoded string");
+    println!("The decoded string is: {}", decoded);
 }
