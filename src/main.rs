027919f1048de9c2a09f69fd662e004f7af65b9b
Hsien-Hsiu Liao <hsien-hsiu.liao@yandex.com>
2022-03-12 09:12:21 -0800

    add decode function

--- src/main.rs
+++ src/main.rs
@@ -1,27 +1,8 @@
+use std::io;
+
 fn main() {
     /*
-    function rot13(str) { // LBH QVQ VG!
-    var num;
-    var newstr="";
-    var decoded="";
-    for (let i=0;i<str.length ;i++){
-      num= str.charCodeAt(i);
-   // num = num-13; a=65 Z=90
-      if (num>77 && num <91){
-    decoded += String.fromCharCode(num-13);
-      //decoded += newstr;
-      }
-      else if (num>64 && num <78){
-    decoded += String.fromCharCode(num+13);
-      //decoded += newstr;
-      }
-      else {
-          decoded += String.fromCharCode(num);
-  
-      }  
-    }
-    return decoded;
-  }
+    
 
   rot13("SERR PBQR PNZC") should decode to the string FREE CODE CAMP
 
@@ -31,7 +12,44 @@ rot13("SERR YBIR?") should decode to the string FREE LOVE?
 
 rot13("GUR DHVPX OEBJA SBK WHZCF BIRE GUR YNML QBT.") should decode to the string THE QUICK BROWN FOX JUMPS OVER THE LAZY DOG.
     */
-    let decoded = String::from("decoded");
+
+
+    let mut encoded = String::new();
     println!("Enter a ROT13 encoded string");
-    println!("The decoded string is: {}", decoded);
+
+    io::stdin()
+        .read_line(&mut encoded)
+        .expect("Failed to read line");
+
+    println!("The decoded string is: {}", decode(&encoded));
+}
+
+fn decode(input: &String) -> String {
+    let mut decoded = String::new();
+    decoded = input.to_string();
+    /* function rot13(str) { // LBH QVQ VG!
+        var num;
+        var newstr="";
+        var decoded="";
+        for (let i=0;i<str.length ;i++){ //iterate over length of string input str
+          num= str.charCodeAt(i);       // get unicode of string at index i and store in num
+       // num = num-13; a=65 Z=90
+          if (num>77 && num <91){
+        decoded += String.fromCharCode(num-13);
+          //decoded += newstr;
+          }
+          else if (num>64 && num <78){
+        decoded += String.fromCharCode(num+13);
+          //decoded += newstr;
+          }
+          else {
+              decoded += String.fromCharCode(num);
+      
+          }  
+        }
+        return decoded;
+        
+      }
+      */
+      decoded
 }
